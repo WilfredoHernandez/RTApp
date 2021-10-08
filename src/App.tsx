58da39958398967746b0af9f8fbec92d43e66836
WilfredoHernandez <wilfredo.munguiafb@gmail.com>
2021-10-08 00:17:47 -0600

    Solving Merge Problems

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
