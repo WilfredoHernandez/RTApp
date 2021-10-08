70621a687f11caa76dddaeb8c6a9a54048c137bd
WilfredoHernandez <wilfredo.munguiafb@gmail.com>
2021-10-08 00:02:52 -0600

    Solving Merge Problems

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home'
 import Register from './pages/Register';
 
 /* Core CSS required for Ionic components to work properly */
@@ -28,13 +27,7 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-      <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/register">
+        <Route exact path="/Register">
           <Register />
         </Route>
       </IonRouterOutlet>
