53338c12d055719d7d3e8c0a8613b7e8f1a0b6ed
RBriones0 <71912409+RBriones0@users.noreply.github.com>
2021-10-07 23:14:10 -0600

    media query 375px

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,40 +4,40 @@ import './Login.scss';
 const Login: React.FC = () => {
     return (
 
-      <IonPage>    
-        <IonContent class="ion-text-center" id="background">
+        <IonPage>
+            <IonContent class="ion-text-center" id="background">
 
-            <div className = "header">
-                <strong> Hello! </strong>
-            </div>
+                <div className="header">
+                    <strong> Hello! </strong>
+                </div>
 
-            <div className = "subHeader">
-                <p> Any tips regarding the application..... </p>
-            </div>
-            <div id = "loginForm">
-                <IonInput className="loginInput"
-                          placeholder="Username">
-                </IonInput>
+                <div className="subHeader">
+                    <p> Any tips regarding the application..... </p>
+                </div>
+                <div id="loginForm">
+                    <IonInput className="loginInput"
+                        placeholder="Username">
+                    </IonInput>
 
-                <IonInput className="loginInput"
-                          placeholder="Password">
-                </IonInput>
-            </div>
-            <div id = "optionsContainer">
-                <IonButton id="loginButton" >Login</IonButton>
-    
-                <IonButton id="forgotPsw" 
-                           routerLink="/Forget" 
-                           fill="clear" > Forgot Password? </IonButton>
-            </div>  
-            <IonButton id="signUp" 
-                       routerLink="/Registry" 
-                       fill="clear" ><p>New here? <span>Sign Up!</span></p></IonButton>
-                       
-            </IonContent>       
+                    <IonInput className="loginInput"
+                        placeholder="Password">
+                    </IonInput>
+                </div>
+                <div id="optionsContainer">
+                    <IonButton id="loginButton" >Login</IonButton>
+
+                    <IonButton id="forgotPsw"
+                        routerLink="/Forget"
+                        fill="clear" > Forgot Password? </IonButton>
+                </div>
+                <IonButton id="signUp"
+                    routerLink="/Registry"
+                    fill="clear" ><p>New here? <span>Sign Up!</span></p></IonButton>
+
+            </IonContent>
         </IonPage>
 
     );
-  };
-  
-  export default Login;
+};
+
+export default Login;
